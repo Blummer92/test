a9c0ba0e389b141006c35ecce5a657a11ce7b4f2
Blummer92 <32861845+Blummer92@users.noreply.github.com>
2019-08-12 23:54:27 -0400

    new commit
    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './styles.css'; // Tell Webpack that Button.js uses these styles
 
-import "./styles.css";
-function App() {
+class click_number extends App {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }}
+
+class Button extends App {
+  render(i) {
+    <Button className="Button" onClick=> allert('click')}>
+    {this.props.value}
+    </Button>
+    // You can use them as regular CSS styles
+  }
+}
+function App(Button) {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <button>
+        this is a button
+      </button>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
 
+
+
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />
+
+  , rootElement);
